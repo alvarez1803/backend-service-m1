bc146c4f0d420a99c4798b7d346d536bcef261d4
alvarez1803 <alejorozco0318@gmail.com>
2024-11-05 09:57:33 -0500

    ajustes de documentacion

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -9,6 +9,15 @@ import {
 
  const productRoutes = Router();
 
+ /**
+  * @swagger
+  * /api/products:
+  *  get:
+  *  summary: obtener todos los productos
+  *  responses:
+  *    200:
+  *       description: Lista de productos
+  */
  productRoutes.get("products/" , getAllProduct);
  productRoutes.get("products/:id" , getProductByID);
  productRoutes.post("products/", createProduct);
